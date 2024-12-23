30fb8a5f382680dc3bf27aaf3b7727e55d881713
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-23 15:14:03 -0300

    DAO Tunel de Conexao JDBC - MySQL

--- src/main/java/com/deveduardo/controle/indexControle.java
+++ src/main/java/com/deveduardo/controle/indexControle.java
@@ -1,6 +1,7 @@
 package com.deveduardo.controle;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,25 +10,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.deveduardo.dao.util.Conexao;
 
 @WebServlet("/publica")
 public class indexControle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-    public indexControle() {
-        super();
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public indexControle() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processarRequisicao(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processarRequisicao(request, response);
 	}
-	
-	private void processarRequisicao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private void processarRequisicao(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String acao = request.getParameter("acao");
 		try {
 			switch (acao) {
@@ -39,12 +43,20 @@ public class indexControle extends HttpServlet {
 			throw new ServletException(ex);
 		}
 	}
-	
+
 	private void novoUsuario(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		Connection conexaoJDBC = Conexao.getConexao();
+		if (conexaoJDBC != null) {
+			System.out.println("Conexao aberta");
+		} else {
+			System.out.println("Sem conexao");
+
+		}
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("publica/publica-novo-usuario.jsp");
 		dispatcher.forward(request, response);
 	}
 
-
-	}
+}
